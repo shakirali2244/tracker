52ddf7df9adf98d301eed3970cac24ee64a5a161
shakir <shakir.ali@innovibe.ca>
2022-05-16 03:19:48 -0700

    init

---
+++ components/TCard.tsx
@@ -0,0 +1,28 @@
+export interface AppProps {
+  T: string;
+  v: number;
+  vw: number;
+  o: number;
+  c: number;
+  h: number;
+  l: number;
+  t: number;
+  n: number;
+}
+
+export interface Obj {
+  obj: AppProps;
+}
+export const TCard = ({ obj }: Obj) => {
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <div className="card-body">
+        <h2 className="card-title">{obj.T}</h2>
+        <p>{obj.v}</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
